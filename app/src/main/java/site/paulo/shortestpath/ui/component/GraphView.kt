e1fdfd4fce243600dbe369bd129d1b7c368359dc
Paulo Fernando <pfernando@valore.com>
2020-02-08 20:17:03 -0300

    Mark start and end point when the user clicks on the cell

--- app/src/main/java/site/paulo/shortestpath/ui/component/GraphView.kt
+++ app/src/main/java/site/paulo/shortestpath/ui/component/GraphView.kt
@@ -3,6 +3,7 @@ package site.paulo.shortestpath.ui.component
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class GraphView : View {
@@ -14,21 +15,13 @@ class GraphView : View {
     private val cols: Int = 5
     private var squareSide: Float = 100f
 
-    private val squares: ArrayList<RectF> = ArrayList()
-
+    private var startPoint: Pair<Int,Int>? = null
+    private var endPoint: Pair<Int,Int>? = null
+    private val squares: HashMap<Pair<Int,Int>, RectF> = HashMap()
     private val paint = Paint()
 
     init {
         configurePaint()
-        visitPosition(0,0)
-        visitPosition(0,1)
-    }
-
-    fun visitPosition(row: Int, col: Int) {
-        if ((row > rows) || (col > cols)) return
-        val topX = squareSide * row
-        val topY = squareSide * col
-        squares.add(RectF(topX, topY, topX + squareSide, topY + squareSide))
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -36,6 +29,54 @@ class GraphView : View {
         drawHorizontalLines(canvas, rows)
         drawVerticalLines(canvas, cols)
         drawNodes(canvas)
+        drawPoints(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!isEnabled) {
+            return false
+        }
+        val x = event.x
+        val y = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                markPoint(getSquareOnPosition(x,y))
+            }
+            MotionEvent.ACTION_MOVE -> {
+
+            }
+            MotionEvent.ACTION_UP -> {
+
+            }
+            MotionEvent.ACTION_CANCEL -> {
+
+            }
+        }
+        return true
+    }
+
+    fun visitPosition(position: Pair<Int, Int>) {
+        squares[position] = getRectInPosition(position)
+        invalidate()
+    }
+
+    private fun getRectInPosition(position: Pair<Int, Int>): RectF {
+        val topX = squareSide * position.first
+        val topY = squareSide * position.second
+        return RectF(topX, topY, topX + squareSide, topY + squareSide)
+    }
+
+    private fun markPoint(position: Pair<Int, Int>) {
+        if (startPoint == null) {
+            startPoint = position
+        } else {
+            if (startPoint == position) {
+                startPoint = null
+            } else {
+                endPoint = position
+            }
+        }
+        invalidate()
     }
 
     private fun drawHorizontalLines(canvas: Canvas, rows: Int) {
@@ -52,10 +93,28 @@ class GraphView : View {
 
     private fun drawNodes(canvas: Canvas) {
         paint.style = Paint.Style.FILL
-        for (node in squares) {
+        for (node in squares.values) {
             canvas.drawRect(node, paint)
         }
         paint.style = Paint.Style.STROKE
+    }
+
+    private fun drawPoints(canvas: Canvas) {
+        paint.style = Paint.Style.FILL
+        if (startPoint != null) {
+            paint.color = Color.BLUE
+            canvas.drawRect(getRectInPosition(startPoint!!), paint)
+        }
+        if (endPoint != null) {
+            paint.color = Color.RED
+            canvas.drawRect(getRectInPosition(endPoint!!), paint)
+        }
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+    }
+
+    private fun getSquareOnPosition(x: Float, y: Float): Pair<Int, Int> {
+        return Pair((x/squareSide).toInt(), (y/squareSide).toInt())
     }
 
     private fun configurePaint() {
