85192f494a9819b3e31baa28ce3d0f6553bf52de
Paulo Fernando <pfernando@valore.com>
2020-02-13 11:43:06 -0300

    Add specs to Djikstra

---
+++ app/src/test/java/site/paulo/pathfinding/algorithm/DjikstraTest.kt
@@ -0,0 +1,77 @@
+package site.paulo.pathfinding.algorithm
+
+import org.junit.jupiter.api.Test
+import site.paulo.pathfinding.data.model.MatrixGraph
+import site.paulo.pathfinding.data.model.Node
+
+internal class DjikstraTest {
+
+    @Test
+    fun `test djikstra algorithm in matrix`() {
+        val matrix = MatrixGraph(3,3)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(2,2))
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 5)
+    }
+
+    @Test
+    fun `test djikstra algorithm in matrix - no path start at corner - remove diagonal`() {
+        val matrix = MatrixGraph(3,3)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(2,2))
+
+        matrix.removeNode(Pair(0,2))
+        matrix.removeNode(Pair(2,0))
+        matrix.removeNode(Pair(1,1))
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 0)
+    }
+
+    @Test
+    fun `test djikstra algorithm in matrix - no path start at corner - lock around start point`() {
+        val matrix = MatrixGraph(3,3)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(2,2))
+
+        matrix.removeNode(Pair(0,1))
+        matrix.removeNode(Pair(1,0))
+        matrix.removeNode(Pair(1,1))
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 0)
+    }
+
+    @Test
+    fun `test djikstra algorithm in matrix - no path - start at middle`() {
+        val matrix = MatrixGraph(10,10)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(5,5))
+
+        matrix.removeNode(Pair(5,4))
+        matrix.removeNode(Pair(5,6))
+        matrix.removeNode(Pair(4,5))
+        matrix.removeNode(Pair(6,5))
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 0)
+    }
+
+    @Test
+    fun `test djikstra algorithm in matrix - short matrix`() {
+        val matrix = MatrixGraph(2,2)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(1,1))
+
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 3)
+    }
+
+    @Test
+    fun `test djikstra algorithm in matrix - shortest matrix`() {
+        val matrix = MatrixGraph(1,1)
+        val djikstra = Djikstra(matrix, Pair(0,0), Pair(0,0))
+
+        djikstra.run()
+
+        assert(djikstra.getPath().size == 1)
+    }
+}
