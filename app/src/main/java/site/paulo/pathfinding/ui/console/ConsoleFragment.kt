3cbfd08cb703f8245a889a4f83a1a02484a38593
Paulo Fernando <pfernando@valore.com>
2020-04-26 14:00:17 -0300

    Adjust console height

--- app/src/main/java/site/paulo/pathfinding/ui/console/ConsoleFragment.kt
+++ app/src/main/java/site/paulo/pathfinding/ui/console/ConsoleFragment.kt
@@ -1,9 +1,11 @@
 package site.paulo.pathfinding.ui.console
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -20,7 +22,25 @@ class ConsoleFragment(private val rows: Array<String>) : BottomSheetDialogFragme
         console.layoutManager = LinearLayoutManager(activity)
         console.adapter = ConsoleAdapter(rows)
 
+
         return root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (view?.parent as View).setBackgroundColor(Color.TRANSPARENT)
+        val resources = resources
+
+        assert(view != null)
+        val parent = view?.parent as View
+        val layoutParams = parent.layoutParams as CoordinatorLayout.LayoutParams
+        layoutParams.setMargins(
+            resources.getDimensionPixelSize(R.dimen.console_margin),
+            0,
+            resources.getDimensionPixelSize(R.dimen.console_margin),
+            0
+        )
+        parent.layoutParams = layoutParams
+
+    }
 }
